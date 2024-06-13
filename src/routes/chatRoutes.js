5f6c1dcfa3cd9d9495aaea60036d61146b16a2a3
Michael Royski <mroyski@gmail.com>
2024-06-13 17:05:43 -0400

    remove routes

--- src/routes/chatRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { createRoom, getMessages } = require('../controllers/chatController');
-const router = express.Router();
-
-router.post('/rooms', createRoom);
-router.get('/rooms/:roomId/messages', getMessages);
-
-module.exports = router;
